8cc4e5e9de9583610187e211e273b701a2ba3a4d
advert31337 <advert31337@yandex.ru>
2019-11-04 19:45:44 +0300

    don`t understand

---
+++ src/app/shared/todos.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs'
+import {tap} from 'rxjs/operators'
+
+
+export interface Todo {
+    id: number
+    title: string
+    completed: boolean
+    date?: any
+}
+
+
+@Injectable({providedIn: 'root'})
+export class TodosService {
+    public todos: Todo[] = [
+        // {id: 1, title: 'Сходить на массаж', completed: false, date: new Date()},
+        // {id: 2, title: 'Позаниматься Angular', completed: true, date: new Date()},
+        // {id: 3, title: 'Посетить концерт', completed: false, date: new Date()},
+    ]
+
+    constructor(private http: HttpClient) {}
+
+    fetchTodos(): Observable<Todo[]> {
+        return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5 ')
+            .pipe(tap( todos  => this.todos = todos))
+    }
+
+    onToggle(id: number) {
+        const idx = this.todos.findIndex(t => t.id === id)
+        this.todos[idx].completed = !this.todos[idx].completed
+      }
+
+    removeTodo(id: number) {
+        this.todos = this.todos.filter(t => t.id !== id)
+    }
+
+
+    addTodo(todo: Todo) {
+        this.todos.push(todo)
+    }
+
+}
